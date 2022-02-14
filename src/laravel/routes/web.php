f3a700ddb2979c70404f168ecaa6762706543fed
hirosedz <tak.boc.228@gmail.com>
2022-02-14 18:15:29 +0900

    web.phpコメントアウト部分削除

--- src/laravel/routes/web.php
+++ src/laravel/routes/web.php
@@ -22,20 +22,6 @@ Route::group(['middleware' => 'auth'], function(){
     Route::get('saunaname', 'SaunaNameController@show')->middleware('auth');
     Route::get('saunaexperience', 'SaunaExperienceController@show')->middleware('auth');
     });
-    // Route::post('news/create','Admin\NewsController@create');
-    // Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
-    // Route::post('profile/create','Admin\ProfileController@create');
-    // Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
-    // Route::post('profile/edit','Admin\ProfileController@update');
-    // Route::get('news', 'Admin\NewsController@index')->middleware('auth');
-    // Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
-    // Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
-    // Route::get('news/delete','Admin\NewsController@delete')->middleware('auth');
-
-
-// Route::get('/user', function () {
-//     return view('user/edit');
-// });
 
 
 // Route::get('/saunaname/new', function() {
